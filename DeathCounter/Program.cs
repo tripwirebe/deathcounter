88ddf3df994d9d1776e83249d82ae53c6cea5c66
tripwires <michel.brouckaert@gmail.com>
2015-03-26 15:26:08 +0100

    inital

--- DeathCounter/Program.cs
+++ DeathCounter/Program.cs
@@ -16,7 +16,7 @@ namespace DeathCounter
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmDeathCounter());
         }
     }
 }
