ceae2c3b9cd17e327eb7bd0f2b5003b9a61d6826
tripwires <michel.brouckaert@gmail.com>
2015-03-27 17:04:54 +0100

    updated version

--- DeathCounter/DeathCounter.cs
+++ DeathCounter/DeathCounter.cs
@@ -102,7 +102,6 @@ namespace DeathCounter
             }
             return deaths;
         }
-
         protected override void WndProc(ref Message m)
         {
             if (m.Msg == 0x0312 && m.WParam.ToInt32() == PLUSHOTKEYID)
