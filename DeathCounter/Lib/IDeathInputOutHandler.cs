f4fe0dce778025ef37aa770911cc41ecb5898656
tripwires <michel.brouckaert@gmail.com>
2016-04-10 14:09:29 +0200

    additional cleanup

--- DeathCounter/Lib/IDeathInputOutHandler.cs
+++ DeathCounter/Lib/IDeathInputOutHandler.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DeathCounter.Lib
+﻿namespace DeathCounter.Lib
 {
     /// <summary>
     /// the signature of input and output handlers
